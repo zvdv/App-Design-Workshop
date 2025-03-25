67f6d743efb86757b1760d046212c8d7c4a4a08c
Zoë van de Vegte <156561301+zvdv@users.noreply.github.com>
2025-03-25 11:09:14 +0600

    Adds detailed comments for inventory

--- inventory/lib/about.dart
+++ inventory/lib/about.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Always needed
 
 class About extends StatelessWidget {
   const About({super.key});
@@ -8,19 +8,18 @@ class About extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 198, 224, 255),
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        // This leading IconButton is automatically added when automaticallyImplyLeading is true (default)
-        leading: IconButton(
+        automaticallyImplyLeading: false, // Tell Flutter not to automatically create the back button on the app bar on a new page
+        leading: IconButton(// This leading IconButton is automatically added when automaticallyImplyLeading is true
           onPressed: (){
-            Navigator.pop(context);
+            Navigator.pop(context); // Pop current page from the navigation stack, bringing us back to our home page
           },
-          icon: Icon(Icons.arrow_back)
+          icon: Icon(Icons.arrow_back) // Back arrow icon
         ),
-        title: Text('About'),
+        title: Text('About'), // Title on app bar
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: Center(
+      body: Center( // Center widget is one way to position a single widget
         child: Text('New information!'),
       ),
     );
--- inventory/lib/main.dart
+++ inventory/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:inventory/about.dart';
+import 'package:flutter/material.dart'; // Always needed
+import 'package:inventory/about.dart'; // Our about page file
 
 void main() {
+  // Main function, program starts here
   runApp(const MainApp());
 }
 
@@ -11,8 +12,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+      debugShowCheckedModeBanner: false, // Disable debug banner in top right corner of running app
+      home: Home(), // Set landing page (we extracted this widget to define it below)
     );
   }
 }
@@ -25,24 +26,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 198, 224, 255),
+      backgroundColor: Color.fromARGB(255, 198, 224, 255), // Background colour for body of app
       appBar: AppBar(
-        title: Text('Inventory'),
+        title: Text('Inventory'), // Title on app bar
         backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        actions: [
+        foregroundColor: Colors.white, // Applies to text and icons on app bar
+        actions: [ // Can add as many actions as you want, will appear on right side of app bar
           IconButton(
             onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>About()));
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>About())); // Push about page onto navigation stack
             },
-            icon: Icon(Icons.info)
+            icon: Icon(Icons.info) // Use info icon from Flutter's default icons
           )
         ],
       ),
-      body: Column(
+      body: Column( // Use a column to layout multiple widgets vertically
         children: [
-          InventoryCard(name: 'Apples'),
-          InventoryCard(name: 'Watermelon'),
+          InventoryCard(name: 'Apples'), // Custom widget we defined and extracted below
+          InventoryCard(name: 'Watermelon'), // We can specify a different name for each instance of this widget
           InventoryCard(name: 'Grapes'),
           InventoryCard(name: 'Bananas')
         ],
@@ -52,11 +53,11 @@ class Home extends StatelessWidget {
 }
 
 class InventoryCard extends StatefulWidget {
-  final String name;
+  final String name; // Create property we can set when we use this widget (this kind of variable can be used in both stateless and stateful widgets)
 
   const InventoryCard({
     super.key,
-    required this.name
+    required this.name // Requires name to be set in order to make the widget
   });
 
   @override
@@ -64,36 +65,36 @@ class InventoryCard extends StatefulWidget {
 }
 
 class _InventoryCardState extends State<InventoryCard> {
-  int quantity = 0;
+  int quantity = 0; // Create state variable (can change without creating a new InventoryCard)
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.amber,
-      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      color: Colors.amber, // Background colour of card
+      margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10), // Space around card
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.all(8.0), // Space within card, around row
+        child: Row( // Use a row to layout multiple widgets horizontally
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // Change alignment of widgets within row
           children: [
-            Text(widget.name),
-            SizedBox(width: 100),
+            Text(widget.name), // Use name property of InventoryCard (need widget. because it's a variable from the widget itself, not the state)
+            SizedBox(width: 100), // Make some space
             IconButton(
               onPressed: (){
-                setState(() {
-                  quantity = quantity - 1;
+                setState(() { // Use setState function to tell Flutter something will need to be updated on the screen
+                  quantity = quantity - 1; // Update the state variable
                 });
               }, 
-              icon: Icon(Icons.remove)
+              icon: Icon(Icons.remove) // Minus icon
             ),
-            Text(quantity.toString()),
+            Text(quantity.toString()), // Use quantity variable (.toString() because quantity is an int)
             IconButton(
               onPressed: (){
                 setState(() {
                   quantity = quantity + 1;
                 });
               },
-              icon: Icon(Icons.add)
+              icon: Icon(Icons.add) // Plus icon
             )
           ],
         ),
